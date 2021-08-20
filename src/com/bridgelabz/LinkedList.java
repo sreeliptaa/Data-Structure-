9b92bd04599523b196d325ffaf47128dc69317a5
sreeliptaa <sreelipta65@gmail.com>
2021-08-20 19:03:15 +0530

    Added: welcome message

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class LinkedList {
+    public static void main(String[] args) {
+        System.out.println("Data Structure Programs using Java Generics");
+    }
+}
