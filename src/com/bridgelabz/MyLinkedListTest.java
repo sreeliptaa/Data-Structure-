ebdb8a057650fc4db71a725b98469a635e424d5e
sreeliptaa <sreelipta65@gmail.com>
2021-08-20 19:35:05 +0530

    Added: add elements in linked list

---
+++ src/com/bridgelabz/MyLinkedListTest.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/** Purpose - create a linked list and add elements
+ * @author - Sreelipta
+ */
+public class MyLinkedListTest {
+    @Test
+    public void given3NumbersWhenAddedToLinkedListShouldBeAddedToTop() {
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.add(mySecondNode);
+        myLinkedList.add(myThirdNode);
+        myLinkedList.printMyNodes();
+        boolean result = myLinkedList.head.equals(myThirdNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myFirstNode);
+        Assertions.assertTrue(result);
+    }
+}
